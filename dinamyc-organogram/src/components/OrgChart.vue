a786f69b1c4b76f7bddd4c6e2fe5e88c70e94edd
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-01-10 14:04:05 -0300

    style: change orgchart link color

--- dinamyc-organogram/src/components/OrgChart.vue
+++ dinamyc-organogram/src/components/OrgChart.vue
@@ -28,7 +28,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="scss">
 html,
 body {
   height: 100%;
@@ -52,5 +52,6 @@ iframe {
   border: none;
   margin: 0;
   padding: 0;
+  /* background-color: $; */
 }
 </style>
