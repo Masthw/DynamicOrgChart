7ea781262698b2e1831a7d33565a97b294cbadb6
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-01-30 10:04:27 -0300

    :sparkles: feat: create home view

---
+++ dinamyc-organogram/src/views/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Bem-vindo à Home Page</h1>
+    <!-- Conteúdo da página inicial -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+};
+</script>
+
+<style scoped>
+/* Estilos específicos da página inicial */
+</style>
