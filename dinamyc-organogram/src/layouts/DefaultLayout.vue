b749818e3be901028550fefb78788c338889cc82
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-01-30 09:57:28 -0300

    :sparkles: feat: create default layout for views

---
+++ dinamyc-organogram/src/layouts/DefaultLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <!-- App Bar aparece em todas as páginas -->
+    <AppBar />
+
+    <!-- Aqui é onde as páginas serão carregadas -->
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import AppBar from 'src/components/AppBar.vue';
+</script>
+
+<style scoped>
+/* Estilos opcionais */
+</style>
