02060736adb3e6923a4d7b2acdfd281a2d3e3784
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-01-30 14:24:10 -0300

    :sparkles: feat: create search component and style

---
+++ dinamyc-organogram/src/components/SearchComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="search-component">
+    <input type="text" placeholder="Buscar Funcionário ou Departamento" />
+    <img src="src/assets/icons/search.png" alt="Buscar" class="search-icon" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchComponent',
+};
+</script>
+
+<style lang="scss" scoped>
+.search-component {
+  margin-left: 30px;
+  position: relative;
+  display: inline-flex;
+  align-items: center;
+  width: 440px;
+  height: 40px;
+  border-radius: 20px;
+  background-color: $white;
+  padding: 5px;
+  font-size: 16px;
+  color: $background-gray;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  outline: none;
+
+  input {
+    flex-grow: 1;
+    border: none;
+    background: transparent;
+    padding-left: 10px;
+    font-size: 16px;
+    color: $gray;
+    &:focus {
+      outline: none;
+    }
+  }
+
+  .search-icon {
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+    cursor: pointer;
+  }
+}
+</style>
