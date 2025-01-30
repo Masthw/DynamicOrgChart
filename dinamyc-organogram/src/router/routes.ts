15deb1208b8721149a1c34ea3e8306ce7ddb7199
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-01-30 10:05:09 -0300

    :sparkles: feat: update app routes

--- dinamyc-organogram/src/router/routes.ts
+++ dinamyc-organogram/src/router/routes.ts
@@ -1,26 +1,29 @@
 import { RouteRecordRaw } from 'vue-router';
-import OrgChartPage from 'src/pages/OrgChartPage.vue';
-import LoginPage from 'src/pages/LoginPage.vue';
-import ResetPasswordPage from 'src/pages/ResetPasswordPage.vue';
-import OrgChartDashboardPage from 'src/pages/OrgChartDashboardPage.vue';
+import OrgChartPage from 'src/views/OrgChartPage.vue';
+import OrgChartDashboardPage from 'src/views/OrgChartDashboardPage.vue';
+import DefaultLayout from 'layouts/DefaultLayout.vue';
+import HomePage from '../views/HomePage.vue';
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: LoginPage,
-  },
-  {
-    path: '/dashboard',
-    component: OrgChartDashboardPage,
-  },
-  {
-    path: '/orgchart/:id',
-    component: OrgChartPage,
-    props: true,
-  },
-  {
-    path: '/reset-password',
-    component: ResetPasswordPage,
+    component: DefaultLayout,
+    children: [
+      {
+        path: '',
+        component: HomePage,
+      },
+
+      {
+        path: '/dashboard',
+        component: OrgChartDashboardPage,
+      },
+      {
+        path: '/orgchart/:id',
+        component: OrgChartPage,
+        props: true,
+      },
+    ],
   },
 ];
 
