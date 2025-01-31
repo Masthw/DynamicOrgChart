b03f4d4c3b390038758182132f531e6d78ee19f0
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-01-31 10:32:50 -0300

    :sparkles: feat: create  button component and style

---
+++ dinamyc-organogram/src/components/ButtonComponent.vue
@@ -0,0 +1,68 @@
+<template>
+  <button :class="['custom-button', variant]" @click="$emit('click')">
+    <q-icon v-if="icon" :name="icon" class="button-icon" size="18px" />
+    <span class="button-text">{{ label }}</span>
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+    default: '',
+  },
+  variant: {
+    type: String,
+    default: 'primary', // 'primary' ou 'secondary'
+    validator: (value) => ['primary', 'secondary'].includes(value),
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.custom-button {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.75rem 1.5rem;
+
+  border-radius: 20px;
+  font-size: 14px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &.primary {
+    background-color: $white;
+    color: $orange;
+    border: 1px solid $orange;
+
+    &:hover {
+      background-color: $orange;
+      color: $white;
+    }
+  }
+
+  &.secondary {
+    background-color: $orange;
+    color: $white;
+    border: 1px solid $orange;
+
+    &:hover {
+      background-color: $white;
+      color: $orange;
+    }
+  }
+
+  .button-icon {
+    margin-right: 0.5rem;
+  }
+
+  .button-text {
+    line-height: 1;
+  }
+}
+</style>
