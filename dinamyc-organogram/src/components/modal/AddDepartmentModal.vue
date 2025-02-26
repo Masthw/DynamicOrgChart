71118f8c7fa13d7328ee7538cc16cddbb045402e
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-02-26 14:39:24 -0300

    :truck: chore: rename form data department name to id

--- dinamyc-organogram/src/components/modal/AddDepartmentModal.vue
+++ dinamyc-organogram/src/components/modal/AddDepartmentModal.vue
@@ -94,7 +94,7 @@ export default {
   data() {
     return {
       formData: {
-        departmentName: '',
+        department_id: '',
         departmentDescription: '',
         departmentColor: '',
       },
